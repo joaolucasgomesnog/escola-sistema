faf8208432d76e821fa0c44be173b5721007993c
joaolucasgomesnog <lucasgomes14598@gmail.com>
2025-05-19 21:12:38 -0300

    started project

--- backend/src/routes/notification.routes.js
+++
@@ -1,9 +0,0 @@
-import notification from "../modules/Notification/index.js";
-import { Router } from "express";
-
-const notificationRoutes = Router()
-
-notificationRoutes.get('/:id', notification.getAllNotificationsByUserId)
-notificationRoutes.delete('/:id', notification.deleteAllNotificationsByUserId)
-
-export { notificationRoutes }
