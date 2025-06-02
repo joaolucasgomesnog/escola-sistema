1ebebc459569e30fbb21de83c1b86bfe3e884280
joaolucasgomesnog <lucasgomes14598@gmail.com>
2025-06-01 21:55:02 -0300

    email added

---
+++ backend/prisma/migrations/20250601233102_email_column_added/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Admin" ADD COLUMN     "email" TEXT;
+
+-- AlterTable
+ALTER TABLE "Student" ADD COLUMN     "email" TEXT;
+
+-- AlterTable
+ALTER TABLE "Teacher" ADD COLUMN     "email" TEXT;
